93eb83605465e9fd7f4e7b81d1ad67f8da7480f1
Gustavo V <gustavo.guly@gmail.com>
2016-09-29 10:45:48 -0300

    implementação facebook login. versão inicial. acerto em como pegar parametro get.

--- bipy3/rest/views.py
+++ bipy3/rest/views.py
@@ -634,7 +634,7 @@ class PageAccessTokenView(views.APIView):
         if nao_valido:
             return nao_valido
         try:
-            fb_acesso = Fb_acesso.objects.get(pk=request.data.get('page_id'))
+            fb_acesso = Fb_acesso.objects.get(pk=request.GET['page_id'])
             logger.debug('===---=-=--=--=-=-= access token::: ' + fb_acesso.page_access_token)
             return Response({"success": True, "access_token": fb_acesso.page_access_token})
         except Fb_acesso.DoesNotExist:
